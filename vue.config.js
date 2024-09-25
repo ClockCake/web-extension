d3a665b535ad6fccd5453673c8e5a3e25f398ef8
ClockCat <hyd1234@outlook.com>
2024-09-25 13:50:06 +0800

    fix: listen port

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // 您可以根据需要更改这个端口
+    allowedHosts: 'all',
+    webSocketServer: false // 这里禁用 WebSocket
+  },
 })
